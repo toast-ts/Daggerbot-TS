c8c539dd2eb91a6f74a9c5afb3a7edd7a28cc25e
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2022-11-22 22:00:04 +1100

    unblock /bonk to be used in general-chat

--- src/commands/bonk.ts
+++ src/commands/bonk.ts
@@ -2,14 +2,14 @@ import Discord,{SlashCommandBuilder} from 'discord.js';
 import { TClient } from 'src/client';
 export default {
     async run(client: TClient, interaction: Discord.ChatInputCommandInteraction<'cached'>){
-        if (!client.isStaff(interaction.member) && interaction.channelId == '468835415093411863') return interaction.reply('This command is restricted to staff only in this channel due to high usage.')
+        //if (!client.isStaff(interaction.member) && interaction.channelId == '468835415093411863') return interaction.reply('This command is restricted to staff only in this channel due to high usage.')
         const member = interaction.options.getUser('member');
         const reason = interaction.options.getString('reason');
         const adminPerm = interaction.member.permissions.has('Administrator')
         if (!member) {
             return interaction.reply('You can\'t bonk the ghost.')
         } else {
-            //if (member && adminPerm) return interaction.reply('You cannot bonk an admin!')
+            //if (member && adminPerm) return interaction.reply('You cannot bonk an admin!') | i need halp with this
             if (member) {
                 const embed = new client.embed().setColor(client.config.embedColor)
                 .setDescription(`> <@${member.id}> has been bonked!\n${reason?.length == null ? '' : `> Reason: **${reason}**`}`)
