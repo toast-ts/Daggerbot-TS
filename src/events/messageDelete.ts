e3df487e6e45b6c0a4201aeca393896e77d72a37
Toast <96593068+AnxietyisReal@users.noreply.github.com>
2022-11-25 20:41:45 +1100

    stop logging deleted msgs plz
    wheres tae when i need him

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -5,9 +5,9 @@ export default {
     execute: async(client:TClient, msg:Discord.Message)=>{
         if (!client.config.botSwitches.logs) return;
         const channel = client.channels.resolve(client.config.mainServer.channels.logs) as Discord.TextChannel;
+        if (msg.guild?.id != client.config.mainServer.id) return;
         if (msg.partial) return;
         if (msg.author.bot) return;
-        if (msg.guildId == client.config.mainServer.id) return;
         const embed = new client.embed().setColor(client.config.embedColorRed).setTimestamp().setAuthor({name: `Author: ${msg.author.tag} (${msg.author.id})`, iconURL: `${msg.author.displayAvatarURL()}`}).setTitle('Message deleted').setDescription(`<@${msg.author.id}>\nContent:\n\`\`\`\n${msg?.content}\n\`\`\`\nChannel: <#${msg.channelId}>`)
         let image;
         if (msg.attachments?.first()?.width && ['png', 'jpeg', 'jpg', 'gif', 'webp'].some(x=>((msg.attachments.first() as Discord.Attachment).name as string).endsWith(x))) {
