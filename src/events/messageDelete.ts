ecbefa08ca0c0b6203da5c78bb2844e1728a5621
toast-ts <96593068+toast-ts@users.noreply.github.com>
2022-11-25 20:37:23 +1100

    why is this not here?

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -7,6 +7,7 @@ export default {
         const channel = client.channels.resolve(client.config.mainServer.channels.logs) as Discord.TextChannel;
         if (msg.partial) return;
         if (msg.author.bot) return;
+        if (msg.channelId == client.config.mainServer.id) return;
         const embed = new client.embed().setColor(client.config.embedColorRed).setTimestamp().setAuthor({name: `Author: ${msg.author.tag} (${msg.author.id})`, iconURL: `${msg.author.displayAvatarURL()}`}).setTitle('Message deleted').setDescription(`<@${msg.author.id}>\nContent:\n\`\`\`\n${msg?.content}\n\`\`\`\nChannel: <#${msg.channelId}>`)
         let image;
         if (msg.attachments?.first()?.width && ['png', 'jpeg', 'jpg', 'gif', 'webp'].some(x=>((msg.attachments.first() as Discord.Attachment).name as string).endsWith(x))) {
