cb018fad8cdb73eb08dffccd4a131b9f53b7f5d1
Toast <96593068+AnxietyisReal@users.noreply.github.com>
2022-12-03 04:38:28 +1100

    bru

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -97,13 +97,13 @@ export default {
                 message.reply('Please don\'t tag Monster unless it\'s important!')
             }
             if (message.content.toLowerCase().startsWith(`*mp players`) || message.content.toLowerCase().startsWith(`*mp status`)){
-                message.reply('Prefix-based MP commands has moved to </mp players:1044732701317537873> and </mp status:1044732701317537873>')
+                message.reply('Prefix-based MP commands has moved to </mp players:1046516411591299233> and </mp status:1046516411591299233>')
             }
             if (message.content.toLowerCase().startsWith(`*lrs`) || message.content.toLowerCase().startsWith(`*rank`)){
                 message.reply('Prefix-based LRS command has moved to </rank view:1044732701317537877>')
             }
             if (message.content.toLowerCase().startsWith(`*mp series`) || message.content.toLowerCase().startsWith(`*mp info`)){
-                message.reply('Prefix-based MP info commands has moved to </mp series:1044732701317537873> and </mp info:1044732701317537873>')
+                message.reply('Prefix-based MP info commands has moved to </mp series:1046516411591299233> and </mp info:1046516411591299233>')
             }
             if (PasswordArray.some(e=>message.content.toLowerCase().includes(e))){
                 message.reply('Password and other details can be found in <#543494084363288637>')
