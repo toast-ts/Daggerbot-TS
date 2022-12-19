a88b32161d548c4e745e3cc4a50bbf15d37d8202
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2022-12-19 15:06:00 +1100

    change out the code like you would with a car battery

--- src/typings/interfaces.d.ts
+++ src/typings/interfaces.d.ts
@@ -30,3 +30,96 @@ export interface Punishment {
     cancels?: number;
     duration?: number;
 }
+export interface DSS_serverName {
+    data: FSData
+}
+export interface FSData {
+    server: FSServer,
+    slots: FSslots
+}
+export interface FSServer {
+    dayTime: number,
+    game: string,
+    mapName: string,
+    mapSize: number,
+    mapOverviewFilename: string,
+    money: number,
+    name: string,
+    server: string,
+    version: string
+}
+export interface FSslots {
+    capacity: number,
+    used: number,
+    players: Array<FSPlayers>
+}
+export interface FSPlayers {
+    isUsed: boolean,
+    isAdmin: boolean,
+    uptime: number,
+    name: string
+}
+export interface FSCareerSavegame {
+    settings: FSCareerSavegameSettings,
+    statistics: FSCareerSavegameStatistics,
+    slotSystem: FSCareerSavegameSlotSystem
+}
+export interface FSCareerSavegameSettings {
+    savegameName: XMLText,
+    creationDate: XMLText,
+    mapId: XMLText,
+    mapTitle: XMLText,
+    saveDataFormatted: XMLText,
+    saveDate: XMLText,
+    resetVehicles: XMLText,
+    trafficeEnabled: XMLText,
+    stopAndGoBraking: XMLText,
+    trailerFillLimit: XMLText,
+    automaticMotorStartEnabled: XMLText,
+    growthMode: XMLText,
+    fixedSeasonalVisuals: XMLText,
+    plannedDaysPerPeriod: XMLText,
+    fruitDestruction: XMLText,
+    plowingRequiredEnabled: XMLText,
+    stonesEnabled: XMLText,
+    weedsEnabled: XMLText,
+    limeRequired: XMLText,
+    isSnowEnabled: XMLText,
+    fuelUsage: XMLText,
+    helperBuyFuel: XMLText,
+    helperBuySeeds: XMLText,
+    helperSlurrySource: XMLText,
+    helperManureSource: XMLText,
+    densityMapRevision: XMLText,
+    terrainTextureRevision: XMLText,
+    terrainLodTextureRevision: XMLText,
+    splitShapesRevision: XMLText,
+    tipCollisionRevision: XMLText,
+    placementCollisionRevision: XMLText,
+    navigationCollisionRevision: XMLText,
+    mapDensityMapRevision: XMLText,
+    mapTerrainTextureRevision: XMLText,
+    mapTerrainLodTextureRevision: XMLText,
+    mapSplitShapesRevision: XMLText,
+    mapTipCollisionRevision: XMLText,
+    mapPlacementCollisionRevision: XMLText,
+    mapNavigationCollisionRevision: XMLText,
+    difficulty: XMLText,
+    economicDifficulty: XMLText,
+    dirtInterval: XMLText,
+    timeScale: XMLText,
+    autoSaveInterval: XMLText
+}
+export interface FSCareerSavegameStatistics {
+    money: XMLText,
+    playTime: XMLText
+}
+export interface FSCareerSavegameSlotSystem {
+    _attributes: slotUsage
+}
+interface slotUsage {
+    slotUsage: string
+}
+interface XMLText {
+    _text: string
+}
