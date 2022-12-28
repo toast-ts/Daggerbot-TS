a5f2cdebb9edd71f9c03db6360be9abeda3bf0ee
Toast <96593068+AnxietyisReal@users.noreply.github.com>
2022-12-29 09:03:57 +1100

    Update botlog.ts

--- src/commands/botlog.ts
+++ src/commands/botlog.ts
@@ -3,7 +3,7 @@ import { TClient } from 'src/client';
 export default {
     async run(client: TClient, interaction: Discord.ChatInputCommandInteraction<'cached'>){
         if (!client.config.eval.whitelist.includes(interaction.user.id)) return client.youNeedRole(interaction, 'bottech');
-        (client.channels.resolve(client.config.mainServer.channels.console) as Discord.TextChannel).send({content: `Uploaded the current console dump as of <t:${Math.round(Date.now()/1000)}:R>`, files: ['../.pm2/logs/Daggerbot-out.log']})
+        (client.channels.resolve(client.config.mainServer.channels.console) as Discord.TextChannel).send({content: `Uploaded the current console dump as of <t:${Math.round(Date.now()/1000)}:R>`, files: ['../../.pm2/logs/Daggerbot-out.log']})
         await interaction.reply('It has been uploaded to dev server.')
     },
     data: new SlashCommandBuilder()
