d7f5f2b9844d8720ef77da9f10c26efd99d81cc5
toast-ts <96593068+toast-ts@users.noreply.github.com>
2022-12-30 07:58:40 +1100

    Provide uptime before restarting

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -4,7 +4,7 @@ import { exec } from 'node:child_process';
 export default {
     async run(client: TClient, interaction: Discord.ChatInputCommandInteraction<'cached'>){
         if (!client.config.eval.whitelist.includes(interaction.user.id)) return client.youNeedRole(interaction, 'bottech');
-        const msg = await interaction.reply({content: 'Pulling...', fetchReply: true})
+        const msg = await interaction.reply({content: 'Pulling from repository...', fetchReply: true})
         exec(
             'git pull',(err:Error,stdout)=>{
                 if (err){
@@ -12,7 +12,7 @@ export default {
                 } else if (stdout.includes('Already up to date')){
                     msg.edit(`Pull aborted:\nUp to date with the repository`)
                 } else {
-                    setTimeout(()=>{msg.edit('Restarting...').then(()=>require('node:child_process').exec('pm2 restart Daggerbot'))},1000)
+                    setTimeout(()=>{msg.edit(`Uptime before restarting: **${client.formatTime(client.uptime as number, 3, {commas: true, longNames: true})}**`).then(()=>require('node:child_process').exec('pm2 restart Daggerbot'))},1000)
                 }
             }
         )
