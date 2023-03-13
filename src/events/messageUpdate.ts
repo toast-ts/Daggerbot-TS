b21dbcc71fa5454473b6b316bff54c90a250eb2d
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2023-03-14 09:24:48 +1100

    Use RegExp for special character matching.

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -5,7 +5,7 @@ export default {
     if (!client.config.botSwitches.logs) return;
     const disabledChannels = ['548032776830582794', '541677709487505408']
     if (oldMsg.guild?.id != client.config.mainServer.id || oldMsg.author == null || oldMsg?.author.bot || oldMsg.partial || newMsg.partial || !newMsg.member || disabledChannels.includes(newMsg.channelId)) return;
-    const msgarr = newMsg.content.toLowerCase().replaceAll('\n', ' ').split(' ');
+    const msgarr = newMsg.content.toLowerCase().replaceAll(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\n]/g, ' ').split(' ');
     if (await client.bannedWords._content.findOne({_id:msgarr}) && (!client.isStaff(newMsg.member))) newMsg.delete();
     if (newMsg.content === oldMsg.content) return;
     (client.channels.resolve(client.config.mainServer.channels.logs) as Discord.TextChannel).send({embeds: [new client.embed().setColor(client.config.embedColor).setTimestamp().setAuthor({name: `Author: ${oldMsg.author.tag} (${oldMsg.author.id})`, iconURL: `${oldMsg.author.displayAvatarURL()}`}).setTitle('Message edited').setDescription(`<@${oldMsg.author.id}>\nOld content:\n\`\`\`\n${oldMsg.content.length < 1 ? '(Attachment)' : oldMsg.content}\n\`\`\`\nNew content:\n\`\`\`\n${newMsg.content}\`\`\`\nChannel: <#${oldMsg.channelId}>`)], components: [new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder().setStyle(5).setURL(`${oldMsg.url}`).setLabel('Jump to message'))]});
