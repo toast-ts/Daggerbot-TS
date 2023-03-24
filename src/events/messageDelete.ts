b508a5aa35782ea36e63b1dbdd573c830db7f433
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2023-03-25 09:47:18 +1100

    Disable edit/delete logs for #things-to-do channel

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -4,7 +4,7 @@ export default {
   run(client:TClient, msg:Discord.Message){
     if (!client.config.botSwitches.logs) return;
     const channel = client.channels.resolve(client.config.mainServer.channels.logs) as Discord.TextChannel;
-    const disabledChannels = [ '548032776830582794', '541677709487505408']
+    const disabledChannels = [ '548032776830582794', '541677709487505408', '949380187668242483']
     if (msg.guild?.id != client.config.mainServer.id || msg.partial || msg.author.bot || disabledChannels.includes(msg.channelId)) return;
     const embed = new client.embed().setColor(client.config.embedColorRed).setTimestamp().setAuthor({name: `Author: ${msg.author.tag} (${msg.author.id})`, iconURL: `${msg.author.displayAvatarURL()}`}).setTitle('Message deleted').setDescription(`<@${msg.author.id}>\n\`${msg.author.id}\``);
     if (msg.content.length != 0) embed.addFields({name: 'Content', value: `\`\`\`\n${msg.content.slice(0,1000)}\n\`\`\``});
