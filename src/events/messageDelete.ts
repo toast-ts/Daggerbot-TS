d58285e9aff19179864a581b70ce4bc9afc479e3
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2023-04-16 07:55:09 +1000

    Not sure where the error came from but ok.

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -6,6 +6,7 @@ export default {
     const channel = client.channels.resolve(client.config.mainServer.channels.logs) as Discord.TextChannel;
     const disabledChannels = [ '548032776830582794', '541677709487505408', '949380187668242483']
     if (msg.guild?.id != client.config.mainServer.id || msg.partial || msg.author.bot || disabledChannels.includes(msg.channelId)) return;
+    if (Discord.DiscordAPIError.name === '10008') return console.log(client.logTime(), 'Caught an unexpected error returned by Discord API. (Unknown Message)');
     const embed = new client.embed().setColor(client.config.embedColorRed).setTimestamp().setAuthor({name: `Author: ${msg.author.tag} (${msg.author.id})`, iconURL: `${msg.author.displayAvatarURL()}`}).setTitle('Message deleted').setDescription(`<@${msg.author.id}>\n\`${msg.author.id}\``);
     if (msg.content.length != 0) embed.addFields({name: 'Content', value: `\`\`\`\n${msg.content.slice(0,1000)}\n\`\`\``});
     embed.addFields(
