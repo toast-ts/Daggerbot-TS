ee7f65b2cbf2d7ee5e7ef5f88e4dc7ecaaeaf31b
toast-ts <96593068+toast-ts@users.noreply.github.com>
2023-04-17 12:57:00 +1000

    Drop the VTC skin faq.

--- src/commands/faq.ts
+++ src/commands/faq.ts
@@ -4,7 +4,6 @@ export default {
   run(client: TClient, interaction: Discord.ChatInputCommandInteraction<'cached'>){
     ({
       srp: ()=>interaction.reply('Ballyspring is the map that is used in Survival Roleplay S4.'),
-      dlskin: ()=>interaction.reply({embeds: [new client.embed().setColor(client.config.embedColor).setTitle('Daggerwin Logistics hex code').setDescription('The main color will be Onyx (`#353839`) with red bumpers.').setImage('https://cdn.discordapp.com/attachments/801965516947324969/806871878736019456/image0.png')]}),
       vtcR: ()=>interaction.reply(`You can get the <@&${client.config.mainServer.roles.vtcmember}> role from <#802283932430106624> by reacting <@282859044593598464>'s message with :truck:\n*VTC skin can also be found in <#801975222609641472> as well.*`),
       mpR: ()=>interaction.reply(`You can get the <@&${client.config.mainServer.roles.mpplayer}> role from <#802283932430106624> by reacting <@282859044593598464>'s message with :tractor:`),
       ytscam: ()=>interaction.reply({embeds: [new client.embed().setColor(client.config.embedColor).setTitle('Scammers in YouTube comments section').setDescription('If you ever see a comment mentioning a giveaway or anything else, **it\'s a scam!**\nYou should report it to YouTube and move on or ignore it.\nP.S: They\'re on every channel and not just Daggerwin.').setImage('https://cdn.discordapp.com/attachments/1015195575693627442/1068078284996345916/image.png')]}),
@@ -22,7 +21,6 @@ export default {
     .setRequired(true)
     .addChoices(
       { name: 'Survival Roleplay Map', value: 'srp'},
-      { name: 'Daggerwin Logistics hex code', value: 'dlskin' },
       { name: 'Scams in YT comments', value: 'ytscam' },
       { name: 'VTC Role', value: 'vtcR' },
       { name: 'MP Role', value: 'mpR' },
