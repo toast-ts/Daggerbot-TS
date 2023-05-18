41b2b7abd599e66f865e5a27540a1dc2bf007b68
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2023-05-18 10:38:56 +1000

    Auto-populate the embed from whitelist instead.

--- src/commands/contributors.ts
+++ src/commands/contributors.ts
@@ -3,15 +3,8 @@ import TClient from '../client.js';
 export default {
   run(client: TClient, interaction: Discord.ChatInputCommandInteraction<'cached'>){
     interaction.reply({embeds: [new client.embed().setColor(client.config.embedColor).setTitle('Daggerbot contributors').setDescription([
-      '**Thanks to those below that contributed to the bot!**',
-      'Toast <@190407856527376384>',
-      'TÆMBØ <@615761944154210305>',
-      'Buzz <@593696856165449749>',
-      'Monster <@215497515934416896>',
-      'RainbowDave <@141304507249197057>',
-      'Hitchhiker <@506022868157595648>',
-      'RedRover92 <@633345781780185099>',
-      'Nawdic <@178941218510602240>'
+      '**Thanks to those below that contributed to/developed the bot!**',
+      `${client.config.whitelist.map(id=>{const member = interaction.guild.members.cache.get(id) as Discord.GuildMember; return `${member.user.username} <@${id}>`}).join('\n')}`
     ].join('\n'))]})
   },
   data: new SlashCommandBuilder()
