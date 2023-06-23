7a41c3e0ba5564bf1271c73c363454541f44cbd9
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2023-06-23 11:28:45 +1000

    Seems to be over now.

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -4,7 +4,7 @@ export default {
   run(client:TClient, msg:Discord.Message){
     if (!client.config.botSwitches.logs) return;
     const channel = client.channels.resolve(client.config.mainServer.channels.logs) as Discord.TextChannel;
-    const disabledChannels = ['548032776830582794', '541677709487505408', '949380187668242483', '1120901022223712296']
+    const disabledChannels = ['548032776830582794', '541677709487505408', '949380187668242483']
     if (msg.guild?.id != client.config.mainServer.id || msg.partial || msg.author.bot || disabledChannels.includes(msg.channelId)) return;
     if (Discord.DiscordAPIError.name === '10008') return console.log(client.logTime(), 'Caught an unexpected error returned by Discord API. (Unknown Message)');
     const embed = new client.embed().setColor(client.config.embedColorRed).setTimestamp().setAuthor({name: `Author: ${msg.author.username} (${msg.author.id})`, iconURL: `${msg.author.displayAvatarURL()}`}).setTitle('Message deleted').setDescription(`<@${msg.author.id}>\n\`${msg.author.id}\``);
