e4fa1abf2f300a5b104e0a10d5213457a09bca4b
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2023-06-25 06:52:23 +1000

    Add steam scam faq

--- src/commands/faq.ts
+++ src/commands/faq.ts
@@ -7,6 +7,7 @@ export default {
       vtcR: ()=>interaction.reply(`You can get the <@&${client.config.mainServer.roles.vtcmember}> role from <#802283932430106624> by clicking :truck: button on a webhook's message\n*VTC skin can also be found in <#801975222609641472> as well.*`),
       mpR: ()=>interaction.reply(`You can get the <@&${client.config.mainServer.roles.mpplayer}> role from <#802283932430106624> by clicking :tractor: button on webhook's message`),
       ytscam: ()=>interaction.reply({embeds: [new client.embed().setColor(client.config.embedColor).setTitle('Scammers in YouTube comments section').setDescription('If you ever see a comment mentioning a giveaway or anything else, **it\'s a scam!**\nYou should report it to YouTube and move on or ignore it.\nP.S: They\'re on every channel and not just Daggerwin.').setImage('https://cdn.discordapp.com/attachments/1015195575693627442/1068078284996345916/image.png')]}),
+      steamscam: ()=>interaction.reply({embeds: [new client.embed().setColor(client.config.embedColor).setTitle('Steam account report scam').setImage('https://cdn.discordapp.com/attachments/1091300529696673792/1122266621088645130/image.png')]}),
       fsShader: ()=>interaction.reply({embeds: [new client.embed().setColor(client.config.embedColor).setTitle('Clearing your shader cache folder').setDescription('If your game keeps crashing shortly after opening your game, then the shaders might be an issue.\nTo resolve this, you can go to `Documents/My Games/FarmingSimulator2022` and delete the folder called `shader_cache`').setImage('https://cdn.discordapp.com/attachments/1015195575693627442/1015195687970943016/unknown.png')]}),
       fsLogfile: ()=>interaction.reply({embeds: [new client.embed().setColor(client.config.embedColor).setTitle('Uploading your log file').setDescription('You can find `log.txt` in `Documents/My Games/FarmingSimulator2022` and upload it into <#596989522395398144> along with your issue, so people can assist you further and help you resolve.').setImage('https://cdn.discordapp.com/attachments/1015195575693627442/1015195643528101958/unknown.png')]}),
       fsDevConsole: ()=>interaction.reply({embeds: [new client.embed().setColor(client.config.embedColor).setTitle('Enabling the development console').setDescription('Head over to `game.xml` in `Documents/My Games/FarmingSimulator2022` and find the section that mentions `<controls>false</controls>` inside development section, change it to `true` then you are good to go!\nFYI: The keybind to open console is \``\u200b\` (backtick).').setImage('https://cdn.discordapp.com/attachments/1015195575693627442/1097273921444790322/image.png')]})
@@ -22,6 +23,7 @@ export default {
     .addChoices(
       { name: 'Survival Roleplay Map', value: 'srp'},
       { name: 'Scams in YT comments', value: 'ytscam' },
+      { name: 'Steam account report scam', value: 'steamscam' },
       { name: 'VTC Role', value: 'vtcR' },
       { name: 'MP Role', value: 'mpR' },
       { name: '[FS22] Resolve shader_cache issue', value: 'fsShader' },
