a59b8f1fdb5ca01a660f62127977ef3f054e2246
toast-ts <96593068+toast-ts@users.noreply.github.com>
2023-08-05 19:56:40 +1000

    Update messageCreate.ts

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -68,7 +68,7 @@ export default {
       const PersonnyMcPerson = `**${message.member.displayName}**`;
       const GeneralChatID = '468835415093411863';
       const incomingArrays = {
-        morning: ['good morning all', 'good morning everyone', 'good morning lads', 'morning all', 'morning everyone', 'morning lads', 'morning guys', 'good morning everybody'],
+        morning: ['good morning all', 'good morning everyone', 'good morning lads', 'morning all', 'morning everyone', 'morning lads', 'morning guys', 'good morning everybody', 'morning yall', 'morning y\'all'],
         afternoon: ['good afternoon', 'afternoon all', 'afternoon everyone'],
         evening: ['good evening', 'evening all', 'evening everyone', 'evening lads'],
         night: ['night all', 'night everyone', 'night guys', 'goodnight', 'good night'],
