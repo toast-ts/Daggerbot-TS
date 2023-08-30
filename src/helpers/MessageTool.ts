9d39a0ef9cf0613078e9feb577c94176c638558f
toast-ts <96593068+toast-ts@users.noreply.github.com>
2023-08-30 10:34:22 +1000

    Add dedicated class for music module

--- src/helpers/MessageTool.ts
+++ src/helpers/MessageTool.ts
@@ -1,6 +1,12 @@
 import Discord from 'discord.js';
 
 export default class MessageTool {
+  static embedMusic(color:Discord.ColorResolvable, title:string, thumbnail?:string, footer?:string){
+    const embed = new Discord.EmbedBuilder().setColor(color).setTitle(title);
+    if (thumbnail) embed.setThumbnail(thumbnail);
+    if (footer) embed.setFooter({text: footer});
+    return embed
+  }
   static embedStruct(color:Discord.ColorResolvable, title:string, description?:string|null, image?:string|null){
     const embed = new Discord.EmbedBuilder().setColor(color).setTitle(title);
     if (description) embed.setDescription(description);
