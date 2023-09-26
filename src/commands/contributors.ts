f2fd51a3f3c25579973e94e8699a1886f66349af
toast-ts <96593068+toast-ts@users.noreply.github.com>
2023-09-26 20:52:56 +1000

    Tweak contributor.ts

--- src/commands/contributors.ts
+++ src/commands/contributors.ts
@@ -5,8 +5,10 @@ export default {
   run(client: TClient, interaction: Discord.ChatInputCommandInteraction<'cached'>){
     interaction.reply({embeds: [new client.embed().setColor(client.config.embedColor).setTitle('Daggerbot contributors').setDescription(MessageTool.concatMessage(
       '**Thanks to those below that contributed to/developed the bot!**',
-      client.config.contribList.map(id=>{const member = interaction.guild.members.cache.get(id); return `${member?.user?.username ?? 'N/A'} <@${id}>`}).join('\n')
-    ))]})
+      client.config.contribList.map(id=>{
+        const member = interaction.guild.members.cache.get(id);
+        return `${member?.user?.username ?? 'N/A'} <@${id}>`}
+      ).join('\n')))]})
   },
   data: new Discord.SlashCommandBuilder()
     .setName('contributors')
