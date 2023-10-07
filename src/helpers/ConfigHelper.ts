969618843baff2b69df602336bba569f6905afef
toast-ts <96593068+toast-ts@users.noreply.github.com>
2023-10-08 08:09:30 +1100

    Remove stuff that aren't supposed to be in production

--- src/helpers/ConfigHelper.ts
+++ src/helpers/ConfigHelper.ts
@@ -5,7 +5,7 @@ export default class ConfigHelper {
     let importconfig:Config;
     try {
       importconfig = JSON.parse(readFileSync(configFile, 'utf8'));
-      console.log(`Using development config :: ${importconfig.configName}`);
+      console.log(`Loaded the config :: ${importconfig.configName}`);
     } catch (e) {
       console.error(`Error loading config file "${configFile}": ${e}`);
       process.exit(1);
