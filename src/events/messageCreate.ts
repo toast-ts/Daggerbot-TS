1076ae56fa9366d767d13d674ef0ab5b4f39bce0
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2023-10-09 09:03:17 +1100

    Bot improvements

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -11,21 +11,20 @@ export default {
     if (!message.inGuild()) return (client.channels.resolve(client.config.mainServer.channels.logs) as Discord.TextChannel).send({content: `<:fish_unamused:1083675172407623711> ${MessageTool.formatMention(client.config.whitelist[0], 'user')}\n**${message.author.username}** tried to send me a DM, their message is:\`\`\`${message.content}\`\`\``, allowedMentions: {parse: ['users']}});
     let automodded: boolean;
 
-    if (client.config.botSwitches.automod && !message.member.roles.cache.has(client.config.mainServer.roles.admin) && message.guildId == client.config.mainServer.id){
+    if (client.config.botSwitches.automod && !message.member.roles.cache.has(client.config.mainServer.roles.admin) && message.guildId === client.config.mainServer.id){
       const automodFailReason = 'msg got possibly deleted by another bot.';
       if (await client.bannedWords._content.findById(Automoderator.scanMsg(message))/*  && !Whitelist.includes(message.channelId) */){
         automodded = true;
         message.delete().catch(()=>Logger.forwardToConsole('log', 'AUTOMOD-BANNEDWORDS', automodFailReason));
         message.channel.send('That word isn\'t allowed here.').then(x=>setTimeout(()=>x.delete(), 10000));
-        await Automoderator.repeatedMessages(client, message, 30000, 3, 'bw', '30m', 'Constant swearing');
+        await Automoderator.repeatedMessages(client, message, 30000, 3, 'bw', '30m', 'Constant swears');
       } else if (message.content.toLowerCase().includes('discord.gg/') && !MessageTool.isStaff(message.member as Discord.GuildMember)){
-        const url = message.content.split(' ').find(x=>x.includes('discord.gg/'));
-        const validInvite = await client.fetchInvite(url).catch(()=>undefined);
+        const validInvite = await client.fetchInvite(message.content.split(' ').find(x=>x.includes('discord.gg/'))).catch(()=>null);
         if (validInvite && validInvite.guild?.id !== client.config.mainServer.id){
           automodded = true;
           message.delete().catch(()=>Logger.forwardToConsole('log', 'AUTOMOD-ADVERT', automodFailReason));
           message.channel.send('Please don\'t advertise other Discord servers.').then(x=>setTimeout(()=>x.delete(), 15000));
-          await Automoderator.repeatedMessages(client, message, 60000, 2, 'adv', '1h', 'Discord advertisement');
+          await Automoderator.repeatedMessages(client, message, 60000, 2, 'adv', '1h', 'Discord Advertisement');
         }
       }
     }
@@ -55,9 +54,8 @@ export default {
       Response.create(client, message, GeneralChatID, 'evening');
       Response.create(client, message, GeneralChatID, 'night');
 
-      CmdTrigger.registerCmds(client, message, '!!_wepanikfrfr');
-      CmdTrigger.MFPwTrigger(message, '!!_farmpw');
-      CmdTrigger.TriggerTest(message, '!!_test-trigger');
+      CmdTrigger.registerCmds(client, message, 'wepanikfrfr');
+      CmdTrigger.MFPwTrigger(message, 'farmpw');
 
       if (message.type === 8 && message.channelId === GeneralChatID) message.channel.send({content: outgoingArrays.guildBoost[Math.floor(Math.random() * outgoingArrays.guildBoost.length)], allowedMentions: {parse: ['users']}})
       if (message.mentions.members.has('309373272594579456') && !MessageTool.isStaff(message.member)) message.reply('Please don\'t tag Daggerwin, read rule 14 in <#468846117405196289>');
--- src/funcs/CmdModule.ts
+++ src/funcs/CmdModule.ts
@@ -2,14 +2,15 @@ import Discord from 'discord.js';
 import TClient from '../client.js';
 
 export default class CmdTrigger {
-  protected static SenseTrigger(message:Discord.Message, trigger:string) {
-    return message.content.toLowerCase().startsWith(trigger)
+  private static readonly prefix = '!!_';
+  private static SenseTrigger(message:Discord.Message, trigger:string): boolean {
+    return message.content.toLowerCase().startsWith(this.prefix+trigger)
   }
   static registerCmds(client:TClient, message:Discord.Message, trigger:string) {
     if (this.SenseTrigger(message, trigger) && client.config.whitelist.includes(message.author.id)) {
       (client.guilds.cache.get(message.guildId) as Discord.Guild).commands.set(client.registry)
-      .then(()=>message.reply('How did you manage to lose the commands??? Anyways, it\'s re-registered now.'))
-      .catch((e:Error)=>message.reply(`Failed to deploy slash commands:\n\`\`\`${e.message}\`\`\``));
+        .then(()=>message.reply('How did you manage to lose the commands??? Anyways, it\'s re-registered now.'))
+        .catch((e:Error)=>message.reply(`Failed to deploy slash commands:\n\`\`\`${e.message}\`\`\``));
     }
   }
   static MFPwTrigger(message:Discord.Message, trigger:string) {
@@ -18,8 +19,5 @@ export default class CmdTrigger {
       if (message.channelId === '1149138133514981386') return message.reply(farmPwText += '`westfarm`')
       else if (message.channelId === '1149138202662293555') return message.reply(farmPwText += '`eastfarm`')
     }
-  }
-  static TriggerTest(message:Discord.Message, trigger:string) {
-    if (this.SenseTrigger(message, trigger)) return message.reply('Triggered!')
   }
 }
