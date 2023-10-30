c4b5984428a808a4cbe8ba6d00d57eefb0d59dcf
toast-ts <96593068+toast-ts@users.noreply.github.com>
2023-10-30 21:34:15 +1100

    Fixed issue caused by PEBCAK

--- src/funcs/Punish.ts
+++ src/funcs/Punish.ts
@@ -13,7 +13,7 @@ export default async(client:TClient, interaction: Discord.ChatInputCommandIntera
   Logger.forwardToConsole('log', 'PunishmentLog', `${GuildMember?.user?.username ?? User?.username ?? 'No user data'} ${time ? ['warn', 'kick'].includes(type) ? 'and no duration set' : `and ${time} (duration)` : ''} was used in /${interaction.commandName} for ${reason}`);
   (client.channels.cache.get(client.config.mainServer.channels.punishment_log) as Discord.TextChannel).send({embeds:[new client.embed().setColor(client.config.embedColor).setAuthor({name: interaction?.user?.username, iconURL: interaction?.user?.displayAvatarURL({size:2048})}).setTitle('Punishment Log').setDescription(`${GuildMember?.user?.username ?? User?.username ?? 'No user data'} ${time ? ['warn', 'kick'].includes(client.punishments.type) ? 'and no duration set' : `and ${time} (duration)` : ''} was used in \`/${interaction.commandName}\` for \`${reason}\``).setTimestamp()]});
   if (interaction.user.id === User.id) return interaction.reply(`You cannot ${type} yourself.`);
-  if (!GuildMember && type != 'unban') return interaction.reply(`You cannot ${type} someone who is not in the server.`);
+  if (!GuildMember && !['unban', 'ban'].includes(type)) return interaction.reply(`You cannot ${type} someone who is not in the server.`);
   if (User.bot) return interaction.reply(`You cannot ${type} a bot!`);
 
   await interaction.deferReply();
