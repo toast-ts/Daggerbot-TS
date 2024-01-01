703f587cc97daf58f78c6fddc7250bcae64bba36
toast-ts <96593068+toast-ts@users.noreply.github.com>
2024-01-02 01:11:05 +1100

    Update articles

--- src/commands/academy.ts
+++ src/commands/academy.ts
@@ -30,9 +30,26 @@ export default class Academy {
           .setTitle(queryFound.embed.title)
           .setURL(`https://www.farming-simulator.com/newsArticle.php?news_id=${queryFound.embed.id}`)
           .setDescription(queryFound.embed.description)
-          .setFooter({text: 'Farming Simulator Academy'})
+          .setFooter({text: 'This information is provided by the contributors with their best FS knowledge. If you find any mistakes, please contact Toast.'})
         ]});
       },
+      how_to_contribute: ()=>interaction.reply({embeds: [new client.embed()
+        .setColor(client.config.embedColor)
+        .setTitle('How to contribute to the Academy')
+        .setDescription(MessageTool.concatMessage(
+          'If you want to show your support and contribute your knowledge to the academy command, you can do so by following the steps below:',
+          '1. Create a [GitHub account](https://github.com/signup) if you haven\'t already',
+          '2. Fork the [GitHub repository](https://github.com/AnxietyisReal/Daggerbot-TS/tree/master)',
+          '3. Navigate to the `articles.yaml` file inside the `src` directory.',
+          '4. Click the pencil icon to edit the file and add your article (**has to be closely related to official FSA article**) using the following format:',
+          '```yaml',
+          '- embed:',
+          '    id: <article_id> # Article ID (/newsArticle.php?news_id=<article_id>)',
+          '    title: <article_title> # Embed title and search term, don\'t make it too long',
+          '    description: <article_description> # Embed description, limit is 4096 and multi-line is supported',
+          '```',
+        )).setFooter({text: 'Or if you\'re not familiar with GitHub, you can contact Toast directly with your article'})
+      ]}),
       update: async()=>{
         if (!client.config.whitelist.includes(interaction.user.id)) return MessageTool.youNeedRole(interaction, 'bottech');
         const articles = await Undici.fetch('https://raw.githubusercontent.com/AnxietyisReal/Daggerbot-TS/master/src/articles.yml').then(x=>x.text());
@@ -56,6 +73,9 @@ export default class Academy {
         .setDescription('The search term')
         .setRequired(true)
         .setAutocomplete(true)))
+    .addSubcommand(x=>x
+      .setName('how_to_contribute')
+      .setDescription('Provides information on how to contribute to the academy command'))
     .addSubcommand(x=>x
       .setName('update')
       .setDescription('Updates the local file with new information from GitHub repository'))
