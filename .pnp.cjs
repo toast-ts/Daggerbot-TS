86ec8d4d8b2f6e94eff59808d619a2418fd24e73
toast-ts <96593068+toast-ts@users.noreply.github.com>
2024-01-01 14:13:49 +1100

    Dedupe @types/node

--- .pnp.cjs
+++ .pnp.cjs
@@ -603,7 +603,7 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./.yarn/cache/@types-continuation-local-storage-npm-3.2.7-763e4dc12b-1f272b53bc.zip/node_modules/@types/continuation-local-storage/",\
         "packageDependencies": [\
           ["@types/continuation-local-storage", "npm:3.2.7"],\
-          ["@types/node", "npm:20.10.5"]\
+          ["@types/node", "npm:20.10.6"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -637,14 +637,6 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@types/node", [\
-      ["npm:20.10.5", {\
-        "packageLocation": "./.yarn/cache/@types-node-npm-20.10.5-9464a4540b-4a378428d2.zip/node_modules/@types/node/",\
-        "packageDependencies": [\
-          ["@types/node", "npm:20.10.5"],\
-          ["undici-types", "npm:5.26.5"]\
-        ],\
-        "linkType": "HARD"\
-      }],\
       ["npm:20.10.6", {\
         "packageLocation": "./.yarn/cache/@types-node-npm-20.10.6-59a7d708ba-08471220d3.zip/node_modules/@types/node/",\
         "packageDependencies": [\
@@ -668,7 +660,7 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./.yarn/cache/@types-pg-npm-8.10.9-ee8620c8e7-787be5431a.zip/node_modules/@types/pg/",\
         "packageDependencies": [\
           ["@types/pg", "npm:8.10.9"],\
-          ["@types/node", "npm:20.10.5"],\
+          ["@types/node", "npm:20.10.6"],\
           ["pg-protocol", "npm:1.6.0"],\
           ["pg-types", "npm:4.0.1"]\
         ],\
@@ -702,7 +694,7 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./.yarn/cache/@types-ws-npm-8.5.9-91d1b2ab07-7cf66383b8.zip/node_modules/@types/ws/",\
         "packageDependencies": [\
           ["@types/ws", "npm:8.5.9"],\
-          ["@types/node", "npm:20.10.5"]\
+          ["@types/node", "npm:20.10.6"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -2733,7 +2725,7 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./.yarn/cache/wkx-npm-0.5.0-fca5152cd8-b8975e33f9.zip/node_modules/wkx/",\
         "packageDependencies": [\
           ["wkx", "npm:0.5.0"],\
-          ["@types/node", "npm:20.10.5"]\
+          ["@types/node", "npm:20.10.6"]\
         ],\
         "linkType": "HARD"\
       }]\
