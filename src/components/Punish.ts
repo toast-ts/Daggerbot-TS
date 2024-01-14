93303cb1d9531229dab27cb8ead85af178c32a5e
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2024-01-14 17:58:25 +1100

    Filter moderator role properly

--- src/components/Punish.ts
+++ src/components/Punish.ts
@@ -2,7 +2,7 @@ import Discord from 'discord.js';
 import TClient from '../client.js';
 import MessageTool from '../helpers/MessageTool.js';
 export default async(client:TClient, interaction: Discord.ChatInputCommandInteraction<'cached'>, type: 'ban'|'softban'|'kick'|'mute'|'warn'|'remind')=>{
-  if (!MessageTool.isStaff(interaction.member)) return MessageTool.youNeedRole(interaction, 'dcmod');
+  if (!MessageTool.isModerator(interaction.member)) return MessageTool.youNeedRole(interaction, 'dcmod');
 
   const time = interaction.options.getString('time') ?? undefined;
   const reason = interaction.options.getString('reason') ?? 'Reason unspecified';
