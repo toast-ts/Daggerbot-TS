84c803671bb7fff8ceb92b23e5aa27f5eb4830b8
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2024-01-15 00:03:05 +1100

    Correct the "uses" property, been there since the start of V2 apparently.

--- src/events/inviteCreate.ts
+++ src/events/inviteCreate.ts
@@ -3,6 +3,6 @@ import TClient from '../client.js';
 export default class InviteCreate {
   static async run(client:TClient, invite:Discord.Invite){
     if (!invite.guild) return;
-    (await (invite.guild as Discord.Guild).invites.fetch()).forEach(inv=>client.invites.set(inv.code,{uses: inv.code, creator: inv.inviterId, channel: inv.channel.name}))
+    (await (invite.guild as Discord.Guild).invites.fetch()).forEach(inv=>client.invites.set(inv.code,{uses: inv.uses, creator: inv.inviterId, channel: inv.channel.name}))
   }
 }
