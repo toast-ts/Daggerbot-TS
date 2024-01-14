9f8976cd072c56f802384615478b502c9044df8b
toast-ts <96593068+toast-ts@users.noreply.github.com>
2024-01-14 19:06:27 +1100

    Remove unused command

--- src/commands/remind.ts
+++ src/commands/remind.ts
@@ -1,12 +1,12 @@
 import Discord from 'discord.js';
 import TClient from '../client.js';
 import Punish from '../components/Punish.js';
-export default class Reminder {
+export default class Remind {
   static run(client: TClient, interaction: Discord.ChatInputCommandInteraction<'cached'>){
     Punish(client, interaction, 'remind');
   }
   static data = new Discord.SlashCommandBuilder()
-    .setName('reminder')
+    .setName('remind')
     .setDescription('Remind a member that they\'re breaking the rules')
     .addUserOption(x=>x
       .setName('member')
