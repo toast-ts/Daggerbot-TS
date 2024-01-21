9f36beb913fcc8b5c283cb47b1c113e2d0291ec9
toast-ts <96593068+toast-ts@users.noreply.github.com>
2024-01-21 23:01:24 +1100

    Add self-incrementing counter

--- ufwReject.sh
+++ ufwReject.sh
@@ -11,6 +11,9 @@ IP_ADDRESSES_STORE="ips.txt"
 # Scan the kernel log for IP addresses and store them in a file
 dmesg | grep 'SRC=' | awk -F'SRC=' '{ print $2 }' | awk '{ print $1 }' | sort | uniq | head -n 5000 > "$IP_ADDRESSES_STORE"
 
+# Initialize a counter for new IPs
+new_ips=0
+
 # Populate the UFW reject rule with the IP addresses collected from the kernel log
 while IFS= read -r ip
 do
@@ -18,8 +21,11 @@ do
   if ! ufw status | grep -q "$ip"
   then
     ufw reject from $ip
+    # Increment the counter
+    ((new_ips++))
   fi
 done < "$IP_ADDRESSES_STORE"
 
 echo "Done populating UFW reject rule"
+echo "$new_ips IP addresses were added"
 exit 0
