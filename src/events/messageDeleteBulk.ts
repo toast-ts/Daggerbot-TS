cf738ffaeb6956f7405b680238d88c0bf22d68e2
toast-ts <96593068+toast-ts@users.noreply.github.com>
2024-02-02 07:12:15 +1100

    Attempt to prevent `undefined: null` in purge log

--- src/events/messageDeleteBulk.ts
+++ src/events/messageDeleteBulk.ts
@@ -3,6 +3,16 @@ import TClient from '../client.js';
 export default class MessageDeleteBulk {
   static run(client:TClient, messages:Discord.Collection<string, Discord.Message<boolean>>, channel:Discord.GuildTextBasedChannel){
     if (!client.config.botSwitches.logs || channel.guildId != client.config.dcServer.id) return;
-    (client.channels.resolve(client.config.dcServer.channels.logs) as Discord.TextChannel).send({embeds: [new client.embed().setColor(client.config.embedColorRed).setTimestamp().setTitle(`${messages.size} messages were purged`).setDescription(`\`\`\`${messages.map(msgs=>`${msgs.author?.username}: ${msgs.content}`).reverse().join('\n').slice(0,3900)}\`\`\``).addFields({name: 'Channel', value: `<#${messages.first().channel.id}>`})]})
+    if (messages.some(msg=>{
+      msg.author?.username === undefined ?? null;
+      msg.content === undefined ?? null;
+    })) return;
+
+    (client.channels.resolve(client.config.dcServer.channels.logs) as Discord.TextChannel).send({embeds: [
+      new client.embed().setColor(client.config.embedColorRed).setTimestamp()
+      .setTitle(`${messages.size} messages were purged`)
+      .setDescription(`\`\`\`${messages.map(msgs=>`${msgs.author?.username}: ${msgs.content}`).reverse().join('\n').slice(0,3900)}\`\`\``)
+      .addFields({name: 'Channel', value: `<#${messages.first().channel.id}>`})
+    ]});
   }
 }
