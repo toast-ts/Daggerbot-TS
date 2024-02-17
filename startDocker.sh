fd3c1bdfff8edbf029dadac4a2cb31335e254c5b
toast-ts <96593068+toast-ts@users.noreply.github.com>
2024-02-18 08:35:15 +1100

    Prevent databases from being killed during update

--- startDocker.sh
+++ startDocker.sh
@@ -2,6 +2,6 @@
 
 docker compose -f docker-compose.bot.yml down && \
 docker compose -f docker-compose.bot.yml build && \
-docker compose -f docker-compose.bot.yml up -d --remove-orphans
+docker compose -f docker-compose.bot.yml up -d
 
 exit 0
