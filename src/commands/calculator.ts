21c2b1031e601df6d4338251d81b84aa6993d74d
toast-ts <96593068+toast-ts@users.noreply.github.com>
2024-02-28 18:18:28 +1100

    Refactor calculator.ts

--- src/commands/calculator.ts
+++ src/commands/calculator.ts
@@ -2,18 +2,22 @@ import Discord from 'discord.js';
 import TClient from '../client.js';
 import MessageTool from '../helpers/MessageTool.js';
 import Formatters from '../helpers/Formatters.js';
+import * as math from 'mathjs';// I hate this, but it doesn't provide a default export though :cimpCopium:
 
 export default class Calculator {
   static run(client: TClient, interaction: Discord.ChatInputCommandInteraction<'cached'>){
     const now = Date.now();
     const exp = interaction.options.getString('expression', true).replace(/[^-()\d/*+.]/g, '');
     try {
-      const result = eval(exp);
+      let result:math.EvalFunction;
       switch (exp) {
         case '1+1':
-          return interaction.reply('2, quick maths.');
+          return interaction.reply('Wh-why are you doing this? I mean... **1+1** is **2**, but... *why?*');
         case '2+2':
-          return interaction.reply('2+2 is 4, minus 1 that\'s 3, quick maths.');
+          return interaction.reply('https://c.tenor.com/5-bc8MtSWCYAAAAC/tenor.gif');
+        case '2+2-1':
+        case '1-2+2':
+          return interaction.reply('https://tenor.com/bhI9r.gif');
         case '0/0':
           return interaction.reply(MessageTool.concatMessage(
             'Imagine that you have zero cookies and you split them evenly among zero friends.',
@@ -21,8 +25,11 @@ export default class Calculator {
             'And Cookie Monster is sad that there are no cookies, and you are sad that you have no friends.',
             '-- Siri, 2015'
           ));
-      };
-      interaction.reply({embeds:[new client.embed().setColor(client.config.embedColor).addFields({name: 'Expression', value: `\`\`\`js\n${exp}\n\`\`\``},{name: 'Answer', value: `\`\`\`js\n${result}\n\`\`\``}).setFooter({text: `Time taken: ${Formatters.timeFormat(Date.now() - now, 3)}`})]})
+        default:
+          result = math.evaluate(exp);
+          break;
+      }
+      if (typeof result !== 'undefined') interaction.reply({embeds:[new client.embed().setColor(client.config.embedColor).addFields({name: 'Expression', value: `\`\`\`js\n${exp}\n\`\`\``},{name: 'Answer', value: `\`\`\`js\n${result}\n\`\`\``}).setFooter({text: `Time taken: ${Formatters.timeFormat(Date.now() - now, 3)}`})]})
     } catch {
       interaction.reply('The given expression is invalid.');
     }
