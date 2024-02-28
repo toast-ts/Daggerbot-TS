af8cbb016cb4539de47248cf327642652fd6fbaa
toast-ts <96593068+toast-ts@users.noreply.github.com>
2024-02-28 20:16:11 +1100

    Update Yarn config and handle macOS paths properly

--- src/helpers/UsernameHelper.ts
+++ src/helpers/UsernameHelper.ts
@@ -1,4 +1,4 @@
 export default (text:string)=>{
-  const dirSlash = process.platform === 'linux' ? '\/' : '\\';
+  const dirSlash = (process.platform === 'linux' || process.platform === 'darwin') ? '\/' : '\\';
   return text?.replace(/(?<=\/Users\/|\/home\/|\/media\/)[^/]+/g, match=>'･'.repeat(match.length)).split(dirSlash).join(dirSlash);
 }
