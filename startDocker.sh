b9ac812c6af7c255b5862e5106c3a931f262ed75
toast-ts <96593068+toast-ts@users.noreply.github.com>
2024-04-03 18:30:23 +1100

    Update dependencies

--- startDocker.sh
+++ startDocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker compose -f docker-compose.bot.yml down && \
 docker compose -f docker-compose.bot.yml build && \
+docker compose -f docker-compose.bot.yml down && \
 docker compose -f docker-compose.bot.yml up -d
 
 exit 0
