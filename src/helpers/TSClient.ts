e9af56c19cee555e35707ad063585f376c5dfa25
toast-ts <96593068+toast-ts@users.noreply.github.com>
2024-04-26 15:34:43 +1000

    Fix TokenService client not making contact

--- src/helpers/TSClient.ts
+++ src/helpers/TSClient.ts
@@ -1,2 +1,2 @@
 import TokenService from '@toast/tokenservice-client';
-export default async()=>new TokenService(process.argv[3] ?? 'daggerbot', false).connect();
+export default async()=>new TokenService(process.argv[3] ?? 'daggerbot').connect();
