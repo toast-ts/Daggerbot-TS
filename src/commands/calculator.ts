024ef0114ecb01553fe1a83da30e8462ca8d4111
toast-ts <96593068+toast-ts@users.noreply.github.com>
2024-11-25 14:09:40 +1100

    Update deps

--- src/commands/calculator.ts
+++ src/commands/calculator.ts
@@ -23,7 +23,7 @@ export default class Calculator {
             'Imagine that you have zero cookies and you split them evenly among zero friends.',
             'How many cookies does each person get? See? It doesn\'t make sense.',
             'And Cookie Monster is sad that there are no cookies, and you are sad that you have no friends.',
-            '-- Siri, 2015'
+            '𑁋 Siri, 2015'
           ));
         default:
           result = math.evaluate(exp);
